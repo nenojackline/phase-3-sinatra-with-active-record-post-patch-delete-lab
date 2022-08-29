bfd509dc7285bc35a8958c1dabe1ec5b1f2feaa6
jackline chepngeno <chepngenojackline0@gmail.com>
2022-08-29 11:25:05 +0300

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,5 +22,21 @@ class ApplicationController < Sinatra::Base
     baked_good = BakedGood.by_price.first
     baked_good.to_json
   end
+  post '/baked_goods' do
+    baked_good = BakedGood.create(params)
+    baked_good.to_json
+  end
 
+  patch '/bakeries/:id' do
+
+    bakery = Bakery.find(params[:id])
+    bakery.update(params)
+    bakery.to_json
+  end
+
+  delete '/baked_goods/:id' do
+    baked_good = BakedGood.find(params[:id])
+    baked_good.destroy
+    {message: "Baked good deleted."}.to_json
+  end
 end
